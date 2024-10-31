578977ee0bf9145813ae7ef5eeb347996111a637
AB_HOST <AB_HOST>
2024-10-31 17:51:40 +0800

    fix: 修复部分界面警告

--- app/projects/watch240/functions/func_setting_sub_about.c
+++ app/projects/watch240/functions/func_setting_sub_about.c
@@ -14,7 +14,8 @@
 
 extern void func_debug_info_check();
 
-typedef struct f_about_t_ {
+typedef struct f_about_t_
+{
     bool flag_drag;                 //开始拖动
     s32 x_pos;
     s32 y_pos;
@@ -47,7 +48,7 @@ compo_form_t *func_set_sub_about_form_create(void)
     uteModulePlatformGetBleMacAddress(Ble_Address);//获取蓝牙地址
 
     snprintf((char *)Ble_Address_str_buf, sizeof(Ble_Address_str_buf), "%02x:%02x:%02x:%02x:%02x:%02x",\
-    Ble_Address[0],Ble_Address[1],Ble_Address[2],Ble_Address[3],Ble_Address[4],Ble_Address[5]); //信息
+             Ble_Address[0],Ble_Address[1],Ble_Address[2],Ble_Address[3],Ble_Address[4],Ble_Address[5]); //信息
 
     //新建窗体
     compo_form_t *frm = compo_form_create(true);
@@ -100,7 +101,7 @@ compo_form_t *func_set_sub_about_form_create(void)
     compo_textbox_set_pos(txt,GUI_SCREEN_CENTER_X/16,GUI_SCREEN_CENTER_Y*1.5 - SHAPE_HEIGTH/2.5);
 
     txt = compo_textbox_create(frm,sizeof(Ble_Address_str_buf));
-    compo_textbox_set(txt, Ble_Address_str_buf);
+    compo_textbox_set(txt, (char*)Ble_Address_str_buf);
     compo_textbox_set_align_center(txt,false);
     compo_textbox_set_pos(txt,GUI_SCREEN_CENTER_X/16,GUI_SCREEN_CENTER_Y*1.5);
 
@@ -134,22 +135,23 @@ static void func_set_sub_about_message(size_msg_t msg)
     f_about_t *slp = (f_about_t *)func_cb.f_cb;
     int id = compo_get_button_id();
 
-    switch (msg) {
+    switch (msg)
+    {
 
-    case MSG_CTP_CLICK:
-         if(id == 1)
-         func_directly_back_to();
-        break;
-    case MSG_CTP_SHORT_UP:
-    case MSG_CTP_SHORT_DOWN:
-        slp->flag_drag = true;
+        case MSG_CTP_CLICK:
+            if(id == 1)
+                func_directly_back_to();
+            break;
+        case MSG_CTP_SHORT_UP:
+        case MSG_CTP_SHORT_DOWN:
+            slp->flag_drag = true;
 
-    case KU_DELAY_BACK:
-        break;
+        case KU_DELAY_BACK:
+            break;
 
-    default:
-		func_message(msg);
-        break;
+        default:
+            func_message(msg);
+            break;
     }
 }
 
@@ -171,7 +173,8 @@ void func_set_sub_about(void)
 {
     printf("%s\n", __func__);
     func_set_sub_about_enter();
-    while (func_cb.sta == FUNC_SET_SUB_ABOUT) {
+    while (func_cb.sta == FUNC_SET_SUB_ABOUT)
+    {
         //func_set_sub_about_process();
         func_set_sub_about_message(msg_dequeue());
     }

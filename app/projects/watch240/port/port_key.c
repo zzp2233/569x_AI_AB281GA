e827e5c010d704bab9b43c800d81601b7f7d6bb5
Wang.Luo <wang.luo@ute-tech.com.cn>
2024-11-12 17:13:49 +0800

    增加一个按键

--- app/projects/watch240/port/port_key.c
+++ app/projects/watch240/port/port_key.c
@@ -5,7 +5,8 @@
 AT(.com_text.bsp.key)
 bool key_multi_press_check_cb(u16 key_return)
 {
-    if (key_return == KU_BACK) {    //仅PWRKEY的BACK按键需要双击功能
+    if (key_return == KU_BACK)      //仅PWRKEY的BACK按键需要双击功能
+    {
         return true;
     }
     return false;
@@ -14,7 +15,8 @@ bool key_multi_press_check_cb(u16 key_return)
 
 #if USER_ADKEY
 AT(.com_rodata.adkey.table)
-const adkey_tbl_t adkey_table[] = {
+const adkey_tbl_t adkey_table[] =
+{
     {0x0A, KEY_BACK},               //BACK          0R
     {0x20, KEY_LEFT},               //LEFT          1K
     {0x35, KEY_RIGHT},              //RIGHT         2K
@@ -25,9 +27,10 @@ const adkey_tbl_t adkey_table[] = {
 #if USER_PWRKEY
 ///最多支持5个按键。数组元数总数请保持不变。不需要的按键改为NO_KEY
 AT(.com_rodata.pwrkey.table)
-const adkey_tbl_t pwrkey_table[6] = {
+const adkey_tbl_t pwrkey_table[6] =
+{
     {0x08, KEY_BACK},               //LEFT          0R
-    {0x3A, NO_KEY},              //RIGHT         12K
+    {0x3A, KEY_RIGHT},              //RIGHT         12K
     {0x7B, NO_KEY},               //BACK          47K
     {0xFF, NO_KEY},
 };
@@ -47,15 +50,21 @@ u8 get_iokey(void)
 {
     u8 key_val = NO_KEY;
 
-    if (!(GPIOF & BIT(1))) {
+    if (!(GPIOF & BIT(1)))
+    {
         key_val = KEY_BACK;                 //KEY0
-    } else if (!(GPIOF & BIT(2))) {
+    }
+    else if (!(GPIOF & BIT(2)))
+    {
         key_val = KEY_LEFT;                 //KEY1
-    } else {
+    }
+    else
+    {
         GPIOFDIR &= ~BIT(1);
         GPIOFCLR = BIT(1);
         delay_us(6);
-        if (!(GPIOF & BIT(2))) {
+        if (!(GPIOF & BIT(2)))
+        {
             key_val = KEY_RIGHT;            //两IO的中间键KEY2
         }
         GPIOFDIR |= BIT(1);

942a8d72aee2985b8cfe752cb09c63724bf7f99e
AB_HOST <AB_HOST>
2024-11-20 18:50:55 +0800

    fix: 切换任意菜单，单次转动编码器概率性抖动或者不灵敏问题; i2c err导致OTA不成功问题

--- app/platform/bsp/bsp_sys.c
+++ app/platform/bsp/bsp_sys.c
@@ -28,7 +28,9 @@ extern u32 __bss_start, __bss_end, __bss_size;
 
 void sd_detect(void);
 void tbox_uart_isr(void);
-
+#if USER_IO_QEDC_EN
+void bsp_qdec_io_process(void);
+#endif
 static u8 heap_func[HEAP_FUNC_SIZE] AT(.heap.func);
 
 #if TRACE_EN
@@ -243,7 +245,9 @@ void usr_tmr1ms_isr(void)
 #endif // LED_DISP_EN
 
     bsp_saradc_tmr1ms_process();
-
+#if USER_IO_QEDC_EN
+    bsp_qdec_io_process();
+#endif
     plugin_tmr1ms_isr();
 }
 

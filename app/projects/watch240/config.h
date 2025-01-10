4c2d47b85daf396a97c3a15eb99f1402e30081c2
Wang.Luo <wang.luo@ute-tech.com.cn>
2025-01-10 11:25:01 +0800

    1、增加项目AB281B； 2、睡眠心率传值随机+-2； 3、修改分区，加大code区

--- app/projects/watch240/config.h
+++ app/projects/watch240/config.h
@@ -37,7 +37,7 @@
 #define CHIP_PACKAGE_SELECT             UTE_CHIP_PACKAGE_SELECT                  //芯片封装选择 5691G/5691C_F
 
 #define GUI_SELECT                      DISPLAY_UTE//GUI_TFT_320_385_GV9B71                 //GUI Display Select
-#define CTP_SELECT                      CTP_CST8X                   //CTP Select
+#define CTP_SELECT                      UTE_DRV_CTP_SELECT                   //CTP Select
 
 #if (GUI_SELECT == GUI_TFT_SPI)                                     //当使用spi通用接口时,
 #define GUI_MODE_SELECT                 MODE_3WIRE_9BIT_2LINE             //屏幕接口选择

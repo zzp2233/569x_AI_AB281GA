a88492fdd33960f3cdb457b6927e2c5733dfd292
qiquan.cai <qiquan.cai@ute-tech.com.cn>
2025-01-21 14:06:29 +0800

    修复每日活动小数显示错误，更换消息图标

--- app/projects/watch240/functions/func_activity.c
+++ app/projects/watch240/functions/func_activity.c
@@ -104,7 +104,7 @@ compo_form_t *func_activity_form_create(void)
     compo_textbox_set_forecolor(textbox,KCAL_ARC_COLOR);
 
     memset(txt_buf,'\0',sizeof(txt_buf));
-    snprintf((char *)txt_buf, sizeof(txt_buf),"%d.%d",distance/100, distance%100);///公里数据
+    snprintf((char *)txt_buf, sizeof(txt_buf),"%ld.%02ld",distance/100, distance%100);///公里数据
     textbox = compo_textbox_create(frm,6);
     compo_textbox_set(textbox, txt_buf);
     compo_textbox_set_pos(textbox,GUI_SCREEN_CENTER_X+GUI_SCREEN_CENTER_X/1.6,GUI_SCREEN_HEIGHT/1.5);
@@ -236,7 +236,7 @@ static void func_activity_disp_handle(void)
             compo_textbox_set(textbox_step, txt_buf);
 
             memset(txt_buf,'\0',sizeof(txt_buf));
-            snprintf((char *)txt_buf, sizeof(txt_buf),"%ld.%ld",f_activity->arc_km_value/100, f_activity->arc_km_value%100);
+            snprintf((char *)txt_buf, sizeof(txt_buf),"%ld.%02ld",f_activity->arc_km_value/100, f_activity->arc_km_value%100);
             compo_textbox_set(textbox_km, txt_buf);
 
             memset(txt_buf,'\0',sizeof(txt_buf));

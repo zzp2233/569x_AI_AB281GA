a7326569c057b31c1fe0c9e5b5d8fab98ee78a09
zy <yan.zhang@ute-tech.com.cn>
2025-03-08 16:10:13 +0800

    1.更新两个表盘（还有一个表盘1内存不够放不进去）

--- ute/flashmap/ute_flash_64mbit_1280K_map.h
+++ ute/flashmap/ute_flash_64mbit_1280K_map.h
@@ -9,11 +9,11 @@
 #define UTE_BOOT2_ADDRESS                               0x00001000
 #define UTE_BOOT2_SIZE                                  0x00001000  //4K Bytes
 #define UTE_CODE_ADDRESS                                0x00002000
-#define UTE_CODE_SIZE                                   0x00140000  //1280K Bytes
-#define UTE_UI_ADDRESS                                  0x00142000
-#define UTE_UI_SIZE                                     0x00519000  //5028K Bytes
+#define UTE_CODE_SIZE                                   0x00120000  //1280K Bytes
+#define UTE_UI_ADDRESS                                  0x00122000
+#define UTE_UI_SIZE                                     0x00539000  //5028K Bytes
 #define UTE_OTA_TMP_ADDRESS                             0x0065B000
-#define UTE_OTA_TMP_SIZE                                0x000A0000  //832K Bytes  UTE_CODE_SIZE*0.65
+#define UTE_OTA_TMP_SIZE                                0x000D0000  //832K Bytes  UTE_CODE_SIZE*0.65
 #define UTE_MODULE_WATCHONLINE_START_ADDRESS            0x006FB000
 #define UTE_MODULE_WATCHONLINE_MAX_SIZE                 0x00080000  //512K Bytes
 #define UTE_MODULE_FILESYSTEM_START_ADDRESS             0x0077B000

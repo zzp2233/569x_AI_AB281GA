20e8d3273c49396725a447dbcb93b470c3dc58be
Wang.Luo <wang.luo@ute-tech.com.cn>
2025-03-20 14:33:49 +0800

    1、修改mute操作,把延时改成定时，运行中断mute操作； 2、修改心率预警界面

--- app/platform/bsp/bsp_sys.h
+++ app/platform/bsp/bsp_sys.h
@@ -219,6 +219,9 @@ typedef struct
     bool sys_init_complete; //系统初始化完成标志，用于互斥ute task和sys task，防止sys没初始化完成就执行ute task导致流程错误
 
     bool need_wakeup_flag;
+
+    bool loudspeaker_mute_flag; //mute 标志位
+    u8 loudspeaker_mute_countdown; //mute 倒计时
 } sys_cb_t;
 extern sys_cb_t sys_cb;
 extern volatile int micl2gnd_flag;
--- app/projects/watch240/functions/func_heart_warning.c
+++ app/projects/watch240/functions/func_heart_warning.c
@@ -49,7 +49,7 @@ compo_form_t *func_heart_warning_form_create(void)
 
     textbox = compo_textbox_create(frm, strlen(i18n[STR_HEART_HIGHT]));
     compo_textbox_set_location(textbox,GUI_SCREEN_CENTER_X,26/2+168,230,widget_text_get_max_height());
-    if(uteModuleHeartGetMaxHeartValue() < uteModuleHeartGetHeartValue())
+    if(uteModuleHeartGetHeartValue() >= uteModuleHeartGetMaxHeartValue())
     {
         compo_textbox_set(textbox,i18n[STR_HEART_HIGHT]);
     }
@@ -57,7 +57,6 @@ compo_form_t *func_heart_warning_form_create(void)
     {
         compo_textbox_set(textbox,i18n[STR_HEART_LOW]);
     }
-    compo_setid(textbox,COMPO_ID_TEXT_UINT);
 
     picbox = compo_picturebox_create(frm, UI_BUF_I330001_REPEAT_HEART_BIN);
     compo_picturebox_set_pos(picbox, 11+140, 98+14);
@@ -73,16 +72,7 @@ static void func_heart_warning_updata(void)
     snprintf(txt_buf, sizeof(txt_buf), "%d", uteModuleHeartGetHeartValue());
     compo_textbox_set(textbox, txt_buf);
 
-    textbox = compo_getobj_byid(COMPO_ID_TEXT_UINT);
-    if (uteModuleHeartGetHeartValue() > uteModuleHeartGetMaxHeartValue())
-    {
-        compo_textbox_set(textbox, i18n[STR_HEART_HIGHT]);
-    }
-    else if (uteModuleHeartGetHeartValue() > uteModuleHeartGetMinHeartValue())
-    {
-        compo_textbox_set(textbox, i18n[STR_HEART_LOW]);
-    }
-    else
+    if (uteModuleHeartGetHeartValue() < uteModuleHeartGetMaxHeartValue() && uteModuleHeartGetHeartValue() > uteModuleHeartGetMinHeartValue())
     {
         uteModuleGuiCommonGoBackLastScreen();
     }

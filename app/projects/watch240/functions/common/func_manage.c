27b3883b1a5996c8a006098c9114fbda51969df8
qiquan.cai <qiquan.cai@ute-tech.com.cn>
2025-04-29 20:58:50 +0800

    添加压力、压力说明、情绪、情绪说明功能界面、修复测试bug

--- app/projects/watch240/functions/common/func_manage.c
+++ app/projects/watch240/functions/common/func_manage.c
@@ -244,9 +244,6 @@ const static func_item_info_t func_item_tbl[] =
 #if UTE_MODULE_SCREENS_GAME_SUPPORT
     {.func_sta=FUNC_GAME,                     .res_addr=UI_BUF_I335001_2_HONEYCOMB_GAME_BIN,                  .str_idx=STR_GAME},                 //游戏
 #endif // UTE_MODULE_SCREENS_GAME_SUPPORT
-// #if UTE_MODULE_SCREENS_VOICE_SUPPORT
-    {.func_sta=FUNC_VOICE,                    .res_addr=UI_BUF_I335001_2_HONEYCOMB_TOOLBOX_BIN,               .str_idx=STR_VOICE},                 //工具箱
-// #endif // UTE_MODULE_SCREENS_VOICE_SUPPORT
 #if UTE_MODULE_SCREENS_SETTING_SUPPORT
     {.func_sta=FUNC_SETTING,                  .res_addr=UI_BUF_I335001_2_HONEYCOMB_SETTINGS_BIN,              .str_idx=STR_SETTING},               //设置
 #endif // UTE_MODULE_SCREENS_SETTING_SUPPORT
@@ -254,18 +251,25 @@ const static func_item_info_t func_item_tbl[] =
     {.func_sta=FUNC_WOMEN_HEALTH,             .res_addr=UI_BUF_I335001_2_HONEYCOMB_PERIOD_BIN,                .str_idx=STR_WOMEN_HEALTH},          //女性健康
 // #endif // UTE_MODULE_SCREENS_HEARTRATE_SUPPORT
 #if UTE_MODULE_SCREENS_STOPWATCH_SUPPORT
-    {.func_sta=STR_STOP_WATCH,                .res_addr=UI_BUF_I335001_2_HONEYCOMB_STOPWATCH_BIN,             .str_idx = FUNC_STOPWATCH},            //秒表
-#endif // UTE_MODULE_SCREENS_STOPWATCH_SUPPORT
+    {STR_STOP_WATCH,                    UI_BUF_I335001_27_MORE_1_LIST_ICON_PIC56X56_X16_Y12_Y80_Y148_Y216_00_STOPWATCH_BIN,       .func_sta = FUNC_STOPWATCH},    //秒表
+#endif
 #if UTE_MODULE_SCREENS_TIMER_SUPPORT
-    {.func_sta=STR_TIMER,                     .res_addr=UI_BUF_I335001_2_HONEYCOMB_TIMER_BIN,                 .str_idx = FUNC_TIMER},                //定时器
-#endif // UTE_MODULE_SCREENS_TIMER_SUPPORT
-    {.func_sta=STR_FIND_PHONE,                .res_addr=UI_BUF_I335001_2_HONEYCOMB_FIND_PHONE_BIN,            .str_idx = FUNC_FINDPHONE},            //查找手机
-    {.func_sta=STR_FLASHLIGHT,                .res_addr=UI_BUF_I335001_2_HONEYCOMB_FLASHLIGHT_BIN,            .str_idx = FUNC_FLASHLIGHT},           //手电筒
+    {STR_TIMER,                  UI_BUF_I335001_27_MORE_1_LIST_ICON_PIC56X56_X16_Y12_Y80_Y148_Y216_01_TIMER_BIN,        .func_sta = FUNC_TIMER},                //计时器
+#endif
+#if UTE_MODULE_SCREENS_FIND_PHNOE_SUPPORT
+    {STR_FIND_PHONE,                UI_BUF_I335001_27_MORE_1_LIST_ICON_PIC56X56_X16_Y12_Y80_Y148_Y216_03_FINDPHONE_BIN,             .func_sta = FUNC_FINDPHONE},  //找手机
+#endif
+#if UTE_MODULE_SCREENS_FLASHLIGHT_SUPPORT
+    {STR_FLASHLIGHT,                     UI_BUF_I335001_27_MORE_1_LIST_ICON_PIC56X56_X16_Y12_Y80_Y148_Y216_02_FLASHLIGHT_BIN,           .func_sta = FUNC_FLASHLIGHT},    //手电筒
+#endif
 #if UTE_MODULE_SCREENS_CAMERA_SUPPORT
-    {.func_sta=STR_CAMERA,                    .res_addr=UI_BUF_I335001_2_HONEYCOMB_REMOTE_CAMERA_BIN,         .str_idx = FUNC_CAMERA},               //相机
-#endif // UTE_MODULE_SCREENS_CAMERA_SUPPORT
+    {STR_CAMERA,                UI_BUF_I335001_27_MORE_1_LIST_ICON_PIC56X56_X16_Y12_Y80_Y148_Y216_04_CAMERA_BIN,              .func_sta = FUNC_CAMERA},  //遥控拍照
+#endif
 #if UTE_MODULE_SCREENS_CALCULATOR_SUPPORT
-    {.func_sta=STR_CALCULATOR,               .res_addr=UI_BUF_I335001_2_HONEYCOMB_CALCULATOR_BIN,             .str_idx = FUNC_CALCULATOR},           //计算器
+    {STR_CALCULATOR,               UI_BUF_I335001_27_MORE_1_LIST_ICON_PIC56X56_X16_Y12_Y80_Y148_Y216_06_CALCULATOR_BIN,          .func_sta = FUNC_CALCULATOR}, //计算器
+#endif
+#if UTE_MODULE_SCREENS_TOOLBOX_SUPPORT
+    {.func_sta=FUNC_TOOLBOX,                 .res_addr=UI_BUF_I335001_2_HONEYCOMB_TOOLBOX_BIN,                .str_idx = STR_TOOL_BOX},           //工具箱
 #endif // UTE_MODULE_SCREENS_CALCULATOR_SUPPORT
 };
 #else
@@ -618,6 +622,7 @@ const static u8 latest_allowed_id[] =
 //    FUNC_OFF,                           //关机
     FUNC_SET_SUB_ABOUT,                 //关于
     FUNC_WOMEN_HEALTH,
+    FUNC_TOOLBOX,
 };
 
 /**

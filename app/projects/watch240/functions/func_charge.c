5a4d8ffc0384f6e17b728bfcf9fbd017affd0788
xiaoqing.huang <xiaoqing.huang@ute-tech.com.cn>
2025-05-06 17:31:54 +0800

    共鑫W21Y项目添加充电、长按关机、侧边栏界面

--- app/projects/watch240/functions/func_charge.c
+++ app/projects/watch240/functions/func_charge.c
@@ -21,6 +21,7 @@ typedef struct f_charge_t_
     u8 percent_bkp;
 } f_charge_t;
 #define BAT_PERCENT_VALUE       uteDrvBatteryCommonGetLvl()  //电量百分比数值
+#define BAT_PERCENT_FLAG        uteDrvBatteryCommonGetChargerStatus()   //充电状态0：未充电，1：充电中，2：充电完成
 #if GUI_SCREEN_SIZE_240X284RGB_I330001_SUPPORT
 
 enum
@@ -989,6 +990,78 @@ compo_form_t *func_charge_form_create(void)
     compo_picturebox_set_pos(picbox, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y+20);
     compo_setid(picbox,COMPO_ID_BAT_PIC);
     compo_picturebox_set_visible(picbox,BAT_PERCENT_VALUE>20);
+
+
+    return frm;
+}
+#elif GUI_SCREEN_SIZE_360X360RGB_I338001_SUPPORT
+enum
+{
+    //数字
+    COMPO_ID_NUM_BAT_VALUE=1,
+    COMPO_ID_TTXT,
+    COMPO_ID_GIF_PIC,
+    COMPO_ID_BAT_PIC,
+};
+
+//电量更新
+void func_charge_update(void)
+{
+    printf("%s,batt:%d,batt_flag:%d",__func__,BAT_PERCENT_VALUE,BAT_PERCENT_FLAG);
+    compo_textbox_t *txt_val   = compo_getobj_byid(COMPO_ID_NUM_BAT_VALUE);
+    compo_textbox_t *txt_title = compo_getobj_byid(COMPO_ID_TTXT);
+    compo_picturebox_t *pic    = compo_getobj_byid(COMPO_ID_BAT_PIC);
+
+    char txt_buf[30];
+    memset(txt_buf,0,sizeof(txt_buf));
+    snprintf(txt_buf,sizeof(txt_buf),"%d%%",BAT_PERCENT_VALUE);
+    compo_textbox_set_location(txt_val,GUI_SCREEN_CENTER_X,BAT_PERCENT_VALUE<=20 ? 169 : 191,150,50);
+    compo_textbox_set(txt_val,txt_buf);
+    compo_textbox_set_visible(txt_title,BAT_PERCENT_VALUE<=20 && BAT_PERCENT_FLAG == BAT_STATUS_NO_CHARGE);
+    compo_picturebox_set_visible(pic,(BAT_PERCENT_VALUE<=20 && BAT_PERCENT_FLAG == BAT_STATUS_NO_CHARGE) || (BAT_PERCENT_VALUE==100 && BAT_PERCENT_FLAG == BAT_STATUS_CHARGED));
+
+}
+
+//创建充电窗体，创建窗体中不要使用功能结构体 func_cb.f_cb
+compo_form_t *func_charge_form_create(void)
+{
+    char txt_buf[30];
+    //新建窗体和背景
+    compo_form_t *frm = compo_form_create(true);
+
+    compo_form_set_mode(frm, COMPO_FORM_MODE_SHOW_TITLE | COMPO_FORM_MODE_SHOW_TIME);
+    compo_form_set_title(frm, i18n[STR_NULL]);
+
+    //充电中动画
+    compo_animation_t *animation = compo_animation_create(frm, UI_BUF_I338001_CHARGE_GIF_BATT_238X2368_BIN);
+    compo_animation_set_pos(animation, GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y+32);//(360-296)/2
+    compo_animation_set_radix(animation, 8);
+    compo_animation_set_interval(animation, 15);
+    compo_setid(animation,COMPO_ID_GIF_PIC);
+    compo_animation_set_visible(animation,BAT_PERCENT_FLAG == BAT_STATUS_CHARGING);
+
+    //低电量背景--低电量跟充满电是同一个图标
+    compo_picturebox_t *picbox = compo_picturebox_create(frm, UI_BUF_I338001_CHARGE_LOW_232X290_BIN);
+    compo_picturebox_set_pos(picbox, GUI_SCREEN_CENTER_X,215);//360-290/2
+    compo_setid(picbox,COMPO_ID_BAT_PIC);
+    compo_picturebox_set_visible(picbox,(BAT_PERCENT_VALUE<=20 && BAT_PERCENT_FLAG == BAT_STATUS_NO_CHARGE) || (BAT_PERCENT_VALUE==100 && BAT_PERCENT_FLAG == BAT_STATUS_CHARGED));
+
+    //电量
+    memset(txt_buf,0,sizeof(txt_buf));
+    snprintf(txt_buf,sizeof(txt_buf),"%d%%",BAT_PERCENT_VALUE);
+    compo_textbox_t *textbox = compo_textbox_create(frm, 5);
+    compo_textbox_set_font(textbox,UI_BUF_0FONT_FONT_NUM_32_BIN);
+    compo_textbox_set_location(textbox,GUI_SCREEN_CENTER_X,191,150,50);
+    compo_textbox_set(textbox,txt_buf);
+    compo_setid(textbox,COMPO_ID_NUM_BAT_VALUE);
+
+    //低电量文本
+    textbox = compo_textbox_create(frm, strlen(i18n[STR_LOW_BATTERY]));
+    compo_textbox_set_location(textbox,GUI_SCREEN_CENTER_X,216,150,30);
+    compo_textbox_set_forecolor(textbox,COLOR_RED);
+    compo_textbox_set(textbox,i18n[STR_LOW_BATTERY]);
+    compo_setid(textbox,COMPO_ID_TTXT);
+    compo_textbox_set_visible(textbox,BAT_PERCENT_VALUE<=20 && BAT_PERCENT_FLAG == BAT_STATUS_NO_CHARGE);
 
 
     return frm;

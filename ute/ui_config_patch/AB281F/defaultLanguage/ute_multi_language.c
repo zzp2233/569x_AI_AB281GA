684f002e5fa894ae6fe5de326d1e6f243ad4827c
xiaoqing.huang <xiaoqing.huang@ute-tech.com.cn>
2025-05-08 11:49:49 +0800

    更改同乐达T70底版本为同乐达T60

--- ute/ui_config_patch/AB281F/defaultLanguage/ute_multi_language.c
+++ ute/ui_config_patch/AB281F/defaultLanguage/ute_multi_language.c
@@ -4,13 +4,9 @@
 
 #include "ute_multi_language_en_rus.h"
 
-#include "ute_multi_language_ar_ril.h"
-
 #include "ute_multi_language_ru_rru.h"
 
 #include "ute_multi_language_tr_rtr.h"
-
-#include "ute_multi_language_ms_rmy.h"
 
 #include "ute_multi_language_ja_rjp.h"
 
@@ -20,15 +16,11 @@
 
 #include "ute_multi_language_fr_rfr.h"
 
-#include "ute_multi_language_in_rid.h"
-
 #include "ute_multi_language_it_rit.h"
 
 #include "ute_multi_language_pt_rpt.h"
 
 #include "ute_multi_language_th_rth.h"
-
-#include "ute_multi_language_vi_rvn.h"
 
 #include "ute_multi_language_zh_rtw.h"
 

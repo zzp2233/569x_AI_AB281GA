f5ec4bc51c92ca2cbaca0a654ab58f66f0cc1896
qiquan.cai <qiquan.cai@ute-tech.com.cn>
2025-05-19 19:28:57 +0800

    更新G22Z项目屏驱

--- ute/project_config/ute_project_config_ab281j.h
+++ ute/project_config/ute_project_config_ab281j.h
@@ -10,7 +10,7 @@
 #define _UTE_PROJECT_CONFIG_AB281D_H_
 
 #define DEFAULT_BLE_DEV_NAME "G28Z"
-#define UTE_SW_VERSION "AB281EV000058"
+#define UTE_SW_VERSION "AB281JV000058"
 /*! 指定资源路径,如果不定义则使用对应项目号的路径,wang.luo 2025-01-07 */
 #define UTE_UI_CONFIG_PATCH "AB281EA"
 

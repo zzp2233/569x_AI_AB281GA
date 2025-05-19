ab3faa36ad9c9ae0b204ecc88dabcbbe02bebbd4
jun.li <xinsheng.liu@ute-tech.com.cn>
2025-05-19 14:59:59 +0800

    新加AB281H资源文件夹，新加ab281db和ab281h项目

--- ute/project_config/ute_project_config.h
+++ ute/project_config/ute_project_config.h
@@ -14,13 +14,15 @@
 #define PROJECT_AB281AB_SUPPORT      0 /*! 同乐达 T69,在T62基础上增加编码器,TFT 240X284 NV3030B,CHIP_5691C_F,wang.luo 2025-04-18 */
 #define PROJECT_AB281B_SUPPORT       0 /*! 整机 801, TFT 240x282 NV3030B,CHIP_5691C_F,wang.luo 2025-01-10 */
 #define PROJECT_AB281C_SUPPORT       0 /*! 同乐达 T60, TFT 360x360 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-01-21 */
-#define PROJECT_AB281D_SUPPORT       1 /*! 共鑫 天之蓝UI, TFT 240x284 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-01-21 */
+#define PROJECT_AB281D_SUPPORT       0 /*! 共鑫 天之蓝UI, TFT 240x284 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-01-21 */
 #define PROJECT_AB281AC_SUPPORT      0 /*! 同乐达T66，TFT 240x296 NV3030B,CHIP_5691C_F 基于AB281AV0000031修改分辨率为240x296 ,ganqian.yi 2025-4-21*/
 #define PROJECT_AB281E_SUPPORT       0 /*! 共鑫 天之蓝UI, TFT 360x360 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-04-21 */
-#define PROJECT_AB281DA_SUPPORT      0/*! 共鑫W17Y,在AB281D基础上更改蓝牙名, TFT 240x284 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-01-21 */
+#define PROJECT_AB281DA_SUPPORT      0 /*! 共鑫W17Y,在AB281D基础上更改蓝牙名, TFT 240x284 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-01-21 */
 #define PROJECT_AB281F_SUPPORT       0 /*! 同乐达T70 TFT 360x360 ST77916,CHIP_5691C_F,xiaoqing.huang 2025-05-08 */
 #define PROJECT_AB281EA_SUPPORT      0 /*! 共鑫 天之蓝UI, TFT 360x360 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-04-21 */
 #define PROJECT_AB281G_SUPPORT       0 /*! 天之蓝UI, TFT 368x448 ,CHIP_5691G,banghua.yin 2025-05-15 */
+#define PROJECT_AB281H_SUPPORT       0 /*! 天之蓝UI, TFT 240*240 ,CHIP_5691G,jun.li 2025-05-19 */
+#define PROJECT_AB281DB_SUPPORT      1 /*! 共鑫W26Y,在AB281DA基础上更改蓝牙名,提供亮度等级 TFT 240x284 NV3030B,CHIP_5691C_F, jun.li 2025-05-19 */
 
 #if PROJECT_AB281_SUPPORT
 #include"ute_project_config_ab281.h"
@@ -38,6 +40,8 @@
 #include "ute_project_config_ab281ac.h"
 #elif PROJECT_AB281DA_SUPPORT
 #include"ute_project_config_ab281da.h"
+#elif PROJECT_AB281DB_SUPPORT
+#include"ute_project_config_ab281db.h"
 #elif PROJECT_AB281E_SUPPORT
 #include"ute_project_config_ab281e.h"
 #elif PROJECT_AB281F_SUPPORT
@@ -46,6 +50,8 @@
 #include"ute_project_config_ab281ea.h"
 #elif PROJECT_AB281G_SUPPORT
 #include"ute_project_config_ab281g.h"
+#elif PROJECT_AB281H_SUPPORT
+#include"ute_project_config_ab281h.h"
 #endif
 /** Log模块控制开关*/
 #define UTE_MODULE_LOG_SUPPORT 0

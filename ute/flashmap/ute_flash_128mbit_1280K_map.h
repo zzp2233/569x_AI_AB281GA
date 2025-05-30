8224217e9b4c98311d119c256ae345f8dfb2b571
Wang.Luo <wang.luo@ute-tech.com.cn>
2025-05-30 11:04:00 +0800

    1、增加128M Flash分区

--- ute/flashmap/ute_flash_128mbit_1280K_map.h
+++ ute/flashmap/ute_flash_128mbit_1280K_map.h
@@ -11,16 +11,16 @@
 #define UTE_CODE_ADDRESS                                0x00002000
 #define UTE_CODE_SIZE                                   0x00140000  //1280K Bytes
 #define UTE_UI_ADDRESS                                  0x00142000
-#define UTE_UI_SIZE                                     0x0073D000  //7492KB Bytes
-#define UTE_OTA_TMP_ADDRESS                             0x0087F000
+#define UTE_UI_SIZE                                     0x00B68000  //11680KB Bytes
+#define UTE_OTA_TMP_ADDRESS                             0x00CAA000
 #define UTE_OTA_TMP_SIZE                                0x000D0000  //832K Bytes  UTE_CODE_SIZE*0.65
-#define UTE_MODULE_WATCHONLINE_START_ADDRESS            0x0094F000
-#define UTE_MODULE_WATCHONLINE_MAX_SIZE                 0x00100000  //1024K Bytes
-#define UTE_MODULE_FILESYSTEM_START_ADDRESS             0x00A4F000
+#define UTE_MODULE_WATCHONLINE_START_ADDRESS            0x00D7A000
+#define UTE_MODULE_WATCHONLINE_MAX_SIZE                 0x00180000  //1536K Bytes
+#define UTE_MODULE_FILESYSTEM_START_ADDRESS             0x00EFA000
 #define UTE_MODULE_FILESYSTEM_MAX_SIZE                  0x00100000  //1024K Bytes
-#define UTE_USER_PARAM_ADDRESS                          0x00B4F000
+#define UTE_USER_PARAM_ADDRESS                          0x00FFA000
 #define UTE_USER_PARAM_SIZE                             0x00001000  //4K Bytes
-#define UTE_FACTORY_PARAM_ADDRESS                       0x00B50000
+#define UTE_FACTORY_PARAM_ADDRESS                       0x00FFB000
 #define UTE_FACTORY_PARAM_SIZE                          0x00005000  //20K Bytes
 
 #define UTE_SYSTEM_USER_DATA_START_ADDRESS UTE_MODULE_WATCHONLINE_START_ADDRESS

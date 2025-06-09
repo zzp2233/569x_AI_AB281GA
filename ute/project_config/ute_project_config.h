fbd2f164fac2fef49563f1dc4cbbf5ec6e57985f
banghua.yin <banghua.yin@ute-tech.com.cn>
2025-06-09 20:39:46 +0800

    1.新增320*380屏幕驱动 2.新增AB281N版本号适配320*380分辨率

--- ute/project_config/ute_project_config.h
+++ ute/project_config/ute_project_config.h
@@ -15,7 +15,7 @@
 #define PROJECT_AB281B_SUPPORT       0 /*! 整机 801, TFT 240x282 NV3030B,CHIP_5691C_F,wang.luo 2025-01-10 */
 #define PROJECT_AB281C_SUPPORT       0 /*! 同乐达 T60, TFT 360x360 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-01-21 */
 #define PROJECT_AB281D_SUPPORT       0 /*! 共鑫 天之蓝UI, TFT 240x284 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-01-21 */
-#define PROJECT_AB281AC_SUPPORT      1 /*! 同乐达T66，TFT 240x296 NV3030B,CHIP_5691C_F 基于AB281AV0000031修改分辨率为240x296 ,ganqian.yi 2025-4-21*/
+#define PROJECT_AB281AC_SUPPORT      0 /*! 同乐达T66，TFT 240x296 NV3030B,CHIP_5691C_F 基于AB281AV0000031修改分辨率为240x296 ,ganqian.yi 2025-4-21*/
 #define PROJECT_AB281E_SUPPORT       0 /*! 共鑫 天之蓝UI, TFT 360x360 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-04-21 */
 #define PROJECT_AB281DA_SUPPORT      0 /*! 共鑫W17Y,在AB281D基础上更改蓝牙名, TFT 240x284 NV3030B,CHIP_5691C_F,quan.qi.cai 2025-01-21 */
 #define PROJECT_AB281F_SUPPORT       0 /*! 同乐达T70 TFT 360x360 ST77916,CHIP_5691C_F,xiaoqing.huang 2025-05-08 */
@@ -27,6 +27,7 @@
 #define PROJECT_AB281K_SUPPORT       0 /*! 共鑫G39Z,在AB281DA基础上更改蓝牙名,表盘,开机logo,TFT 240x284 NV3030B,CHIP_5691C_F,banghua.yin 2025-06-03 */
 #define PROJECT_AB281L_SUPPORT       0 /*! 天之蓝UI, 共鑫240x284，W26Y-A中性定制需求软件 */
 #define PROJECT_AB281M_SUPPORT       0 /*! 天之蓝UI, 共鑫240x284，W26Y-B中性定制需求软件 */
+#define PROJECT_AB281N_SUPPORT       1 /*! 分辨率320*380，Y17 CHIP_5691C_F  2025-06-09*/
 
 #if PROJECT_AB281_SUPPORT
 #include"ute_project_config_ab281.h"
@@ -64,6 +65,8 @@
 #include"ute_project_config_ab281l.h"
 #elif PROJECT_AB281M_SUPPORT
 #include"ute_project_config_ab281m.h"
+#elif PROJECT_AB281N_SUPPORT
+#include"ute_project_config_ab281n.h"
 #endif
 /** Log模块控制开关*/
 #define UTE_MODULE_LOG_SUPPORT 0

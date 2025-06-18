f246c236d7f82df3dfecb1c63809abc80a79b1f6
banghua.yin <banghua.yin@ute-tech.com.cn>
2025-06-18 19:14:25 +0800

    G39Z更新版本AB281KV000108：最新修改

--- ute/project_config/ute_project_config_ab281k.h
+++ ute/project_config/ute_project_config_ab281k.h
@@ -10,7 +10,7 @@
 #define _UTE_PROJECT_CONFIG_AB281K_H_
 
 #define DEFAULT_BLE_DEV_NAME "Aolon GTS"
-#define UTE_SW_VERSION "AB281KV000081"
+#define UTE_SW_VERSION "AB281KV000108"
 
 /*! 指定资源路径,如果不定义则使用对应项目号的路径,wang.luo 2025-01-07 */
 //#define UTE_UI_CONFIG_PATCH "AB281K"
@@ -180,20 +180,18 @@
 /*! 默认表盘索引 zn.zeng, 2021-10-25  */
 #define DEFAULT_WATCH_INDEX 0
 /*! 最大表盘数量，不包括在线表盘 zn.zeng, 2021-10-23  */
-#define UTE_MODULE_SCREENS_WATCH_CNT_MAX 7
+#define UTE_MODULE_SCREENS_WATCH_CNT_MAX 5
 /*! 表盘排序地址数组,wang.luo 2024-11-26 */
 #define UTE_MODULE_WATCHS_SORT_ADDRESS_ARRAYS { UI_BUF_DIALPLATE_G39Z001_BIN, \
                                                 UI_BUF_DIALPLATE_G39Z002_BIN, \
                                                 UI_BUF_DIALPLATE_G39Z003_BIN, \
                                                 UI_BUF_DIALPLATE_G39Z004_BIN, \
                                                 UI_BUF_DIALPLATE_G39Z005_BIN, \
-                                                UI_BUF_DIALPLATE_CUBE_LIGHT_BIN,\
-                                                UI_BUF_DIALPLATE_BTF_BIN,\
                                               }
-//     UI_BUF_DIALPLATE_CUBE_BIN
+//     UI_BUF_DIALPLATE_CUBE_BIN UI_BUF_DIALPLATE_BTF_BIN UI_BUF_DIALPLATE_CUBE_LIGHT_BIN
 
-#define UTE_WATCHS_BUTTERFLY_DIAL_SUPPORT 1     // 使用蝴蝶表盘
-#define UTE_WATCHS_CUBE_DIAL_SUPPORT 1          // 使用立方体表盘
+#define UTE_WATCHS_BUTTERFLY_DIAL_SUPPORT 0     // 使用蝴蝶表盘
+#define UTE_WATCHS_CUBE_DIAL_SUPPORT 0          // 使用立方体表盘
 #define UTE_WATCHS_LIGHT_CUBE_DIAL_SUPPORT 0    // 使用光束立方体表盘
 #define UTE_WATCHS_DIALPLATE_BTF_INDEX 6        // 蝴蝶表盘索引
 #define UTE_WATCHS_DIALPLATE_CUBE_INDEX 5       // 立方体表盘索引

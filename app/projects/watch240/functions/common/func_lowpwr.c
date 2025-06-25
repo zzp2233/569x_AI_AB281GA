2a094e4221b59f9808d2e413e9a4ef6d94b8b2c0
Wang.Luo <wang.luo@ute-tech.com.cn>
2025-06-25 12:03:31 +0800

    1、关闭计步算法切换主频功能，新lib会导致蓝牙断连且功耗更高

--- app/projects/watch240/functions/common/func_lowpwr.c
+++ app/projects/watch240/functions/common/func_lowpwr.c
@@ -344,7 +344,7 @@ static void sfunc_sleep(void)
     adda_clk_source_sel(1);                     //adda_clk48_a select xosc52m
     PLL0CON &= ~(BIT(18) | BIT(6));             //pll0 sdm & analog disable
     rtc_sleep_enter();
-    BTCON2 |= BIT(10) | BIT(8);                 //osc_en_wken 解决新btstack.lib休眠后蓝牙断连
+    // BTCON2 |= BIT(10) | BIT(8);                 //osc_en_wken 解决新btstack.lib休眠后蓝牙断连
 
     //io analog input
     pa_de = GPIOADE;

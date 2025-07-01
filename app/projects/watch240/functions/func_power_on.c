6526570374c2c025daf11ffe92075dc69ac33ccb
qiquan.cai <qiquan.cai@ute-tech.com.cn>
2025-07-01 10:38:55 +0800

    共鑫W23Z[240x296]需求 1、更换电池曲线 2、添加新表盘 3、更换需求功能

--- app/projects/watch240/functions/func_power_on.c
+++ app/projects/watch240/functions/func_power_on.c
@@ -170,6 +170,10 @@ compo_form_t *func_power_on_form_create(void)
     compo_picturebox_cut(pic, 0, 24);
     compo_picturebox_set_pos(pic,GUI_SCREEN_CENTER_X, GUI_SCREEN_CENTER_Y);
     compo_setid(pic, COMPO_PIC_ID);
+#elif UTE_MODULE_SCREENS_POWER_AOLON_SUPPORT
+    compo_picturebox_t *pic = compo_picturebox_create(frm, UI_BUF_I335001_LOGO_LOGO_BIN);///背景图片
+    compo_picturebox_set_pos(pic,GUI_SCREEN_CENTER_X, 140);
+    compo_setid(pic, COMPO_PIC_ID);
 #else
 #ifdef UI_BUF_I335001_LOGO_ICON_LOGO_142X68_X50_Y107_BIN
     compo_picturebox_t *pic = compo_picturebox_create(frm, UI_BUF_I335001_LOGO_ICON_LOGO_142X68_X50_Y107_BIN);///背景图片

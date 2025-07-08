453d26417634d6e66cea47a1b0d5d0c2df76c642
zy <yan.zhang@ute-tech.com.cn>
2025-07-08 11:46:41 +0800

    i语言问题修改和升级后清除数据

--- ute/ui_config_patch/ABE012/defaultLanguage/ute_multi_language_en_rus.h
+++ ute/ui_config_patch/ABE012/defaultLanguage/ute_multi_language_en_rus.h
@@ -3,7 +3,7 @@
 *@brief utf8 words list h file
 *@details language: 英语(美国）
 *@author
-*@date   2025-05-06 15:01:21
+*@date   2025-07-08 11:19:14
 *@version  UTE WordListTool,Version 2.2.1.0
 */
 
--- ute/ui_config_patch/ABE012/defaultLanguage/ute_multi_language_es_res.h
+++ ute/ui_config_patch/ABE012/defaultLanguage/ute_multi_language_es_res.h
@@ -3,7 +3,7 @@
 *@brief utf8 words list h file
 *@details language: 西班牙(西班牙）
 *@author
-*@date   2025-05-06 15:01:22
+*@date   2025-07-08 11:19:16
 *@version  UTE WordListTool,Version 2.2.1.0
 */
 

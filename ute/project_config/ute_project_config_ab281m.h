c8ff60c5021f4ca13115b53624872fa2a0c9b7a5
zhenhaoli <zhenhao.li@ute-tech.com.cn>
2025-07-11 15:01:22 +0800

    1.AB281MV000142更换表盘&补充提交

--- ute/project_config/ute_project_config_ab281m.h
+++ ute/project_config/ute_project_config_ab281m.h
@@ -10,7 +10,7 @@
 #define _UTE_PROJECT_CONFIG_AB281M_H_
 
 #define DEFAULT_BLE_DEV_NAME "T8"
-#define UTE_SW_VERSION "AB281MV000141"
+#define UTE_SW_VERSION "AB281MV000142"
 
 /*! 指定资源路径,如果不定义则使用对应项目号的路径,wang.luo 2025-01-07 */
 #define UTE_UI_CONFIG_PATCH "AB281M"

a53f82981861f8b55895bbc13afb48aa35a965ab
qiquan.cai <qiquan.cai@ute-tech.com.cn>
2025-07-11 15:41:35 +0800

    1、W26Y中性A更换电池曲线

--- ute/project_config/ute_project_config_ab281l.h
+++ ute/project_config/ute_project_config_ab281l.h
@@ -149,20 +149,20 @@
 #endif
 
 /* 电池容量 zn.zeng 2022-01-07*/
-#define UTE_DRV_BATTERY_ELECTRICITY_POWER_MAH      280 //mAh
+#define UTE_DRV_BATTERY_ELECTRICITY_POWER_MAH      230 //mAh
 
 /* 电池曲线 zn.zeng 2022-01-03*/
-#define UTE_DRV_BATTERY_000      3450  //3564
-#define UTE_DRV_BATTERY_010      3678  //3698
-#define UTE_DRV_BATTERY_020      3725  //3744
+#define UTE_DRV_BATTERY_000      3458  //3564
+#define UTE_DRV_BATTERY_010      3679  //3698
+#define UTE_DRV_BATTERY_020      3722  //3744
 #define UTE_DRV_BATTERY_030      3755  //3765
 #define UTE_DRV_BATTERY_040      3773  //3780
-#define UTE_DRV_BATTERY_050      3797  //3807
-#define UTE_DRV_BATTERY_060      3836  //3856
-#define UTE_DRV_BATTERY_070      3902  //3908
-#define UTE_DRV_BATTERY_080      3963  //3964
-#define UTE_DRV_BATTERY_090      4050  //4045
-#define UTE_DRV_BATTERY_100      4126  //4128
+#define UTE_DRV_BATTERY_050      3798  //3807
+#define UTE_DRV_BATTERY_060      3835  //3856
+#define UTE_DRV_BATTERY_070      3903  //3908
+#define UTE_DRV_BATTERY_080      3966  //3964
+#define UTE_DRV_BATTERY_090      4058  //4045
+#define UTE_DRV_BATTERY_100      4144  //4128
 
 // #define UTE_DRV_TP_X_AXIS_EXCHANGE 1 // 交换X轴左右坐标
 // #define UTE_DRV_TP_Y_AXIS_EXCHANGE 1 // 交换Y轴上下坐标

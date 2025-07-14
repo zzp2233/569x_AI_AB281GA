88651d5ade13af48ae4a142300146d704f598385
banghua.yin <banghua.yin@ute-tech.com.cn>
2025-07-14 17:45:37 +0800

    1、AB281J同步JB152版本修改 2、词条同步更换

--- ute/project_config/ute_project_config.h
+++ ute/project_config/ute_project_config.h
@@ -28,7 +28,7 @@
 #define PROJECT_AB281L_SUPPORT       0 /*! 天之蓝UI, 共鑫240x284，W26Y-A中性定制需求软件 */
 #define PROJECT_AB281M_SUPPORT       0 /*! 天之蓝UI, 共鑫240x284，W26Y-B中性定制需求软件 */
 #define PROJECT_AB281N_SUPPORT       0 /*! 分辨率320*380，Y17 CHIP_5691C_F  2025-06-09*/
-#define PROJECT_AB281DC_SUPPORT      1 /*! 恒鑫HX01定制,双按键+编码器+手电筒,TFT 240x296 JD9853,CHIP_5691C_F,wang.luo 2025-06-13 */
+#define PROJECT_AB281DC_SUPPORT      0 /*! 恒鑫HX01定制,双按键+编码器+手电筒,TFT 240x296 JD9853,CHIP_5691C_F,wang.luo 2025-06-13 */
 #define PROJECT_AB281GA_SUPPORT      0 /*! 传音815N,amoled 368x448,wang.luo,CHIP_5691G 2025-06-13 */
 #define PROJECT_AB281FA_SUPPORT      0 /*! 同乐达T70-A TFT 360x360 ST77916,CHIP_5691C_F,xiaoqing.huang 2025-05-08 */
 #define PROJECT_AB281AD_SUPPORT      0 /*! 融创A310,在T62基础上增加编码器,TFT 240X294 NV3030B,CHIP_5691C_F,qiquan.cai 2025-06-21 */

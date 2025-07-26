d46387af32d72ba211846a7a484cb371ab0c833c
zhongpeng.zeng <kunlin.liu@ute-tech.com.cn>
2025-07-26 19:06:10 +0800

    更换图片资源

--- app/projects/watch240/functions/func_chatbot.c
+++ app/projects/watch240/functions/func_chatbot.c
@@ -49,14 +49,14 @@ compo_form_t *func_chatbot_form_create(void)
 
     compo_animation_t *animation = compo_animation_create(frm, UI_BUF_I330001_AI_SPEAKING_BIN);
     compo_animation_set_pos(animation, 120, 148);
-    compo_animation_set_radix(animation, 38);
+    compo_animation_set_radix(animation, 30);
     compo_animation_set_interval(animation, 15);
     compo_setid(animation, COMPO_ID_BTN_SPEAKING);
     compo_animation_set_visible(animation, false);
 
     animation = compo_animation_create(frm, UI_BUF_I330001_AI_SPEAKED_BIN);
     compo_animation_set_pos(animation, 120, 148);
-    compo_animation_set_radix(animation, 38);
+    compo_animation_set_radix(animation, 30);
     compo_animation_set_interval(animation, 15);
     compo_setid(animation, COMPO_ID_BTN_SPEAKED);
     compo_animation_set_visible(animation, false);

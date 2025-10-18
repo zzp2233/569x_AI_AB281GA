351f395c2b7e8211be42e287830a6b67f3a4061c
zhongpeng.zeng <kunlin.liu@ute-tech.com.cn>
2025-10-18 12:06:13 +0800

    Initial commit with all files and history

--- app/platform/modules/yj/lib_et_ui_io.c
+++
@@ -1,8 +0,0 @@
-#include "include.h"
-#include "lib_et_ui_io.h"
-
-#if (ASR_SELECT == ASR_YJ)
-
-ALIGNED(8) ET_UI_IO_T  t_pwn  AT(.asr.buff);
-
-#endif //ASR_SELECT
--- app/projects/watch466/functions/common/func_ota_ui.h
+++
@@ -1,6 +0,0 @@
-#ifndef _FUNC_IDLE_H
-#define _FUNC_IDLE_H
-
-void func_idle_message(u16 msg);
-
-#endif // _FUNC_IDLE_H
